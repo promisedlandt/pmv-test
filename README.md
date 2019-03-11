563c8d917aadca7cb99dad1fb8da53f78a75f9a6
Nils Landt <nils.landt@nlsoft.de>
2019-03-11 09:21:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+git clone https://github.com/promisedlandt/pmv-test /tmp/pmv-test
+cd /tmp/pmv-test
+./setup.sh
+
+vim -u vimrc.local Gemfile
+```
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+mkdir bundle
+git clone https://github.com/carakan/pmv.vim bundle/pmv.vim
+git clone https://github.com/mattn/webapi-vim/ bundle/webapi-vim
+git clone https://github.com/tpope/vim-pathogen bundle/vim-pathogen
